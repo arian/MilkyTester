dbda9567b410f727720d20d7c68891f7172e565f
Arian <arian@aryweb.nl>
2011-10-27 04:10:38 +0200

    Put the logging of colors in a separate file and add some extra info-logs

--- app.js
+++ app.js
@@ -2,9 +2,13 @@
 var app = require('http').createServer(handler),
 	io = require('socket.io').listen(app),
 	fs = require('fs'),
-	repl = require('repl');
+	repl = require('repl'),
+	log = require('./colorLog').colorLog;
 
 app.listen(8080);
+
+log.yellow('- Visit http://localhost:8080 to run the tests');
+log.yellow('- type "test()" to run the tests in all browsers\n');
 
 function handler (req, res) {
 	var url = req.url;
@@ -79,24 +83,5 @@ var prettyLogFailures = function(items, level){
 		}
 	});
 };
-
-var log = {
-	ANSIColors: {
-		green:  '\033[32m',
-		red:    '\033[31m',
-		cyan:   '\033[36m',
-		yellow: '\033[1;33m',
-		normal: '\033[0m'
-	},
-	log: function(msg, color){
-		console.log((this.ANSIColors[color] || this.ANSIColors.normal) + msg + this.ANSIColors.normal);
-	}
-};
-for (var col in log.ANSIColors) (function(col){
-	log[col] = function(msg){
-		this.log(msg, col);
-	};
-})(col);
-
 
 
